55fde9575d837850ad737ea2459e16840de5bf7d
LauriePlisson <plisson.laurie@gmail.com>
2025-08-04 10:16:17 +0200

    uid2 et bcrypt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 const User = require("../models/users");
 const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 // signUp inscription
 router.post("/");
