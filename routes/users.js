22cd7250f2a192a90e20db50823928795676acf2
LauriePlisson <plisson.laurie@gmail.com>
2025-08-07 11:23:36 +0200

    update route users

--- routes/users.js
+++ routes/users.js
@@ -12,47 +12,52 @@ const uniqid = require("uniqid");
 
 // signUp inscription
 router.post("/signup", (req, res) => {
-  if (!checkBody(req.body, ["username", "password", "mail"])) {
-    res.json({ result: false, error: "Missing or empty fields" });
-    return;
+  try {
+    checkBody(req.body, ["username", "password", "mail"]);
+
+    User.findOne({ username: req.body.username }).then((data) => {
+      if (data === null) {
+        const hash = bcrypt.hashSync(req.body.password, 10);
+
+        const newUser = new User({
+          username: req.body.username,
+          mail: req.body.mail,
+          password: hash,
+          token: uid2(32),
+          avatar: "",
+          description: "@" + req.body.username,
+          team: null,
+          friendsList: [],
+          postsList: [],
+        });
+
+        newUser.save().then((data) => {
+          res.json({ result: true, message: data.username + " is created" });
+        });
+      } else {
+        res.json({ result: false, error: "User already exists" });
+      }
+    });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred during sign-up",
+      errorDetails: error.message,
+    });
   }
-
-  User.findOne({ username: req.body.username }).then((data) => {
-    if (data === null) {
-      const hash = bcrypt.hashSync(req.body.password, 10);
-
-      const newUser = new User({
-        username: req.body.username,
-        mail: req.body.mail,
-        password: hash,
-        token: uid2(32),
-        avatar: "",
-        description: "@" + req.body.username,
-        team: null,
-        friendsList: [],
-        postsList: [],
-      });
-
-      newUser.save().then((data) => {
-        res.json({ result: true, message: data.username + " is created" });
-      });
-    } else {
-      res.json({ result: false, error: "User already exists" });
-    }
-  });
 });
 
 //signin connexion
 router.post("/signin", (req, res) => {
-  if (!checkBody(req.body, ["username", "password"])) {
-    res.json({ result: false, error: "Missing or empty fields" });
-    return;
-  }
+  try {
+    checkBody(req.body, ["username", "password"]);
 
-  User.findOne({ username: req.body.username }).then((data) => {
-    if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      User.updateOne({ username: req.body.username }, { token: uid2(32) }).then(
-        (uptdateData) => {
+    User.findOne({ username: req.body.username }).then((data) => {
+      if (data && bcrypt.compareSync(req.body.password, data.password)) {
+        User.updateOne(
+          { username: req.body.username },
+          { token: uid2(32) }
+        ).then((uptdateData) => {
           User.findOne({ username: req.body.username })
             .populate("team")
             .then((finalData) => {
@@ -66,126 +71,177 @@ router.post("/signin", (req, res) => {
                 team: finalData.team,
               });
             });
-        }
-      );
-    } else {
-      res.json({ result: false, error: "User not found or wrong password" });
-    }
-  });
+        });
+      } else {
+        res.json({ result: false, error: "User not found or wrong password" });
+      }
+    });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred during sign-in",
+      errorDetails: error.message,
+    });
+  }
 });
 
 //suppression compte
 router.delete("/", (req, res) => {
-  User.deleteOne({ token: req.body.token }).then((data) => {
-    console.log(data);
-    if (data.deletedCount === 1) {
-      res.json({ result: true, message: "account deleted" });
-    } else {
-      res.json({ result: false, error: "error can not delete account" });
-    }
-  });
+  try {
+    User.deleteOne({ token: req.body.token }).then((data) => {
+      // console.log(data);
+      if (data.deletedCount === 1) {
+        res.json({ result: true, message: "account deleted" });
+      } else {
+        res.json({ result: false, error: "error can not delete account" });
+      }
+    });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while deleting the account",
+      errorDetails: error.message,
+    });
+  }
 });
 
 //get all users
 router.get("/", (req, res) => {
-  User.find()
-    .populate("team")
-    .then((data) => {
-      let users = [];
-      for (let user of data) {
-        users.push({
-          username: user.username,
-          avatar: user.avatar,
-          description: user.description,
-          team: user.team,
-        });
-      }
-      res.json({ result: true, users: users });
+  try {
+    User.find()
+      .populate("team")
+      .then((data) => {
+        let users = [];
+        for (let user of data) {
+          users.push({
+            username: user.username,
+            avatar: user.avatar,
+            description: user.description,
+            team: user.team,
+          });
+        }
+        res.json({ result: true, users: users });
+      });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while fetching users",
+      errorDetails: error.message,
     });
+  }
 });
 
 //put modif username
 router.put("/changeUsername/:username", (req, res) => {
-  if (!checkBody(req.params, ["username"])) {
-    res.json({ result: false, error: "Missing or empty fields" });
-    return;
-  }
-  User.updateOne(
-    { token: req.body.token },
-    { username: req.params.username }
-  ).then((data) => {
-    if (data.modifiedCount > 0) {
-      User.findOne({ token: req.body.token }).then((userdata) => {
-        res.json({ result: true, username: userdata.username });
-      });
-    } else {
-      res.json({ result: false, error: "no modification" });
-    }
-  });
-});
-
-//put modif description
-router.put("/changeDescription", (req, res) => {
-  User.updateOne(
-    { token: req.body.token },
-    { description: req.body.description }
-  ).then((data) => {
-    if (data.modifiedCount > 0) {
-      User.findOne({ token: req.body.token }).then((userdata) => {
-        res.json({ result: true, data: userdata.description });
-      });
-    } else {
-      res.json({ result: false, error: "no modification" });
-    }
-  });
-});
-
-//put modif avatar
-router.put("/changeAvatar", async (req, res) => {
-  const avatarPath = `./tmp/${uniqid()}.jpg`;
-  const resultMove = await req.files.avatarUrl.mv(avatarPath);
-
-  if (!resultMove) {
-    const resultCloudinary = await cloudinary.uploader.upload(avatarPath);
-    fs.unlinkSync(avatarPath);
+  try {
+    checkBody(req.params, ["username"]);
 
     User.updateOne(
       { token: req.body.token },
-      { avatar: resultCloudinary.secure_url }
+      { username: req.params.username }
     ).then((data) => {
       if (data.modifiedCount > 0) {
         User.findOne({ token: req.body.token }).then((userdata) => {
-          res.json({ result: true, data: userdata.avatar });
+          res.json({ result: true, username: userdata.username });
         });
       } else {
         res.json({ result: false, error: "no modification" });
       }
     });
-  } else {
-    res.json({ result: false, error: "File upload failed" });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while updating the username",
+      errorDetails: error.message,
+    });
+  }
+});
+
+//put modif description
+router.put("/changeDescription", (req, res) => {
+  try {
+    User.updateOne(
+      { token: req.body.token },
+      { description: req.body.description }
+    ).then((data) => {
+      if (data.modifiedCount > 0) {
+        User.findOne({ token: req.body.token }).then((userdata) => {
+          res.json({ result: true, data: userdata.description });
+        });
+      } else {
+        res.json({ result: false, error: "no modification" });
+      }
+    });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while updating the description",
+      errorDetails: error.message,
+    });
+  }
+});
+
+//put modif avatar
+router.put("/changeAvatar", async (req, res) => {
+  try {
+    const avatarPath = `./tmp/${uniqid()}.jpg`;
+    const resultMove = await req.files.avatar.mv(avatarPath);
+    if (!resultMove) {
+      const resultCloudinary = await cloudinary.uploader.upload(avatarPath);
+      // console.log("Cloudinary upload result:", resultCloudinary);
+      fs.unlinkSync(avatarPath);
+
+      User.updateOne(
+        { token: req.body.token },
+        { avatar: resultCloudinary.secure_url }
+      ).then((data) => {
+        if (data.modifiedCount > 0) {
+          User.findOne({ token: req.body.token }).then((userdata) => {
+            res.json({ result: true, avatarUrl: userdata.avatar });
+          });
+        } else {
+          res.json({ result: false, error: "no modification" });
+        }
+      });
+    } else {
+      res.json({ result: false, error: "File upload failed" });
+    }
+  } catch (error) {
+    // console.error("Error during avatar upload:", error);
+    res.json({
+      result: false,
+      error: "An error occurred while uploading the avatar",
+      errorDetails: error.message,
+    });
   }
 });
 
 //modif password
 
 router.put("/changePassword", (req, res) => {
-  if (!checkBody(req.body, ["newpassword", "password"])) {
-    res.json({ result: false, error: "Missing or empty fields" });
-    return;
-  }
+  try {
+    checkBody(req.body, ["newpassword", "password"]);
 
-  User.findOne({ token: req.body.token }).then((data) => {
-    if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      const hash = bcrypt.hashSync(req.body.newpassword, 10);
-      User.updateOne({ token: req.body.token }, { password: hash }).then(
-        (data) => {
-          res.json({ result: true, message: "password Changed" });
-        }
-      );
-    } else {
-      res.json({ result: false, error: "error, wrong password" });
-    }
-  });
+    User.findOne({ token: req.body.token }).then((data) => {
+      if (data && bcrypt.compareSync(req.body.password, data.password)) {
+        const hash = bcrypt.hashSync(req.body.newpassword, 10);
+        User.updateOne({ token: req.body.token }, { password: hash }).then(
+          (data) => {
+            res.json({ result: true, message: "password Changed" });
+          }
+        );
+      } else {
+        res.json({ result: false, error: "error, wrong password" });
+      }
+    });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while changing the password",
+      errorDetails: error.message,
+    });
+    // console.error("Error during password change:", error);
+  }
 });
 
 //faire ajout suppression ami
@@ -194,71 +250,88 @@ router.put("/addFriend", (req, res) => {
   //   res.json({ result: false, error: "Missing or empty fields" });
   //   return;
   // }
-
-  User.findOne({ token: req.body.token }).then((data) => {
-    if (data) {
-      User.findOne({ username: req.body.friendUsername }).then((friendData) => {
-        if (
-          !data.friendsList.some(
-            (elem) => elem.toString() === friendData._id.toString()
-          )
-        ) {
-          User.updateOne(
-            { token: req.body.token },
-            { $addToSet: { friendsList: friendData._id } }
-          ).then(() => {
-            User.findOne({ token: req.body.token }).then((userData) => {
-              res.json({
-                result: true,
-                message: "Friend added",
-                friendList: userData.friendsList,
+  try {
+    User.findOne({ token: req.body.token }).then((data) => {
+      if (data) {
+        User.findOne({ username: req.body.friendUsername }).then(
+          (friendData) => {
+            if (
+              !data.friendsList.some(
+                (elem) => elem.toString() === friendData._id.toString()
+              )
+            ) {
+              User.updateOne(
+                { token: req.body.token },
+                { $addToSet: { friendsList: friendData._id } }
+              ).then(() => {
+                User.findOne({ token: req.body.token }).then((userData) => {
+                  res.json({
+                    result: true,
+                    message: "Friend added",
+                    friendList: userData.friendsList,
+                  });
+                });
               });
-            });
-          });
-        } else {
-          console.log("Friend already exists or not found");
-          User.updateOne(
-            { token: req.body.token },
-            { $pull: { friendsList: friendData._id } }
-          ).then(() => {
-            console.log("Friend removed");
-            User.findOne({ token: req.body.token }).then((userData) => {
-              res.json({
-                result: true,
-                message: "Friend removed",
-                friendList: userData.friendsList,
+            } else {
+              console.log("Friend already exists or not found");
+              User.updateOne(
+                { token: req.body.token },
+                { $pull: { friendsList: friendData._id } }
+              ).then(() => {
+                console.log("Friend removed");
+                User.findOne({ token: req.body.token }).then((userData) => {
+                  res.json({
+                    result: true,
+                    message: "Friend removed",
+                    friendList: userData.friendsList,
+                  });
+                });
               });
-            });
-          });
-        }
-      });
-    }
-  });
+            }
+          }
+        );
+      }
+    });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while adding/removing a friend",
+      errorDetails: error.message,
+    });
+  }
 });
 
 //get one user
 router.get("/:username", (req, res) => {
-  console.log("Fetching user:", req.params.username);
-  User.findOne({ username: req.params.username })
-    .populate("team")
-    .populate("postsList")
-    .then((data) => {
-      if (data) {
-        res.json({
-          result: true,
-          username: data.username,
-          avatar: data.avatar,
-          description: data.description,
-          team: data.team,
-          friendList: data.friendsList,
-          numberOfFriends: data.friendsList.length,
-          postsList: data.postsList,
-          numberOfPosts: data.postsList.length,
-        });
-      } else {
-        res.json({ result: false, error: "User not found" });
-      }
+  // console.log("Fetching user:", req.params.username);
+  try {
+    User.findOne({ username: req.params.username })
+      .populate("team")
+      .populate("postsList")
+      .then((data) => {
+        if (data) {
+          res.json({
+            result: true,
+            username: data.username,
+            avatar: data.avatar,
+            description: data.description,
+            team: data.team,
+            friendList: data.friendsList,
+            numberOfFriends: data.friendsList.length,
+            postsList: data.postsList,
+            numberOfPosts: data.postsList.length,
+          });
+        } else {
+          res.json({ result: false, error: "User not found" });
+        }
+      });
+  } catch (error) {
+    res.json({
+      result: false,
+      error: "An error occurred while fetching the user",
+      errorDetails: error.message,
     });
+  }
 });
 
 module.exports = router;
